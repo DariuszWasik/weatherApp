ea654e260ccc07792f3ee9837f5aa62ba725974b
Dario_W <wasik.dariusz24@gmail.com>
2025-12-04 14:08:43 +0100

    file structure + fetch function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 // src/index.js
 import './styles.css';
-import { greeting } from './greeting.js';
+import { searchByLocation } from './searchByLocation.js';
 
-console.log(greeting);
-
-// src/index.js
-import odinImage from './odin.png';
-
-const image = document.createElement('img');
-image.src = odinImage;
-
-document.body.appendChild(image);
+searchByLocation('munich');
---
+++ src/searchByLocation.js
@@ -0,0 +1,12 @@
+export { searchByLocation, allData };
+
+let allData = {};
+
+async function searchByLocation(location) {
+	console.log('jestem w search');
+	const response = await fetch(
+		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=hours,days,current&key=GP4ERC9CCBP7EKM9KA7ZTY6LD&contentType=json`,
+	);
+	allData = await response.json();
+	console.log('allData', allData);
+}
